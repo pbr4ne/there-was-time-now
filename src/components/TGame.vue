77ec306d6bc5c34e02de4e24edc93b804d02ea63
pbr4ne <pbr4ne@protonmail.com>
2022-01-04 21:56:12 -0800

    refactor: move tabs to own component

--- src/components/TGame.vue
+++ src/components/TGame.vue
@@ -3,109 +3,33 @@
     <t-header />
     <n-layout has-sider>
       <t-sider />
-      <n-layout>
-        <n-tabs type="card" justify-content="space-evenly">
-          <n-tab-pane 
-            v-for="person in (personList.filter(person => person.isUnlocked))" 
-            :key="person.key"
-            :name="person.key" 
-            :tab="person.personName"
-          >
-            <n-space horizontal style="padding-left: 20px;">
-              <n-progress
-                type="multiple-circle"
-                :percentage="person.scienceList.filter(science => science.isUnlocked).map(({ current }) => current)"
-                :color="person.scienceList.filter(science => science.isUnlocked).map(({ color }) => color)"
-              >
-                Science
-              </n-progress>
-              <n-space vertical>
-                <n-button 
-                  ghost
-                  round
-                  v-for="science in person.scienceList.filter(science => science.isUnlocked)"
-                  :key="science.key"
-                  :color="science.color"
-                  @click="increment(science.key)"
-                >
-                  {{science.label}}
-                </n-button>
-              </n-space>
-            </n-space>
-            <n-space vertical style="padding: 20px;">
-              <!-- <n-badge value=5> -->
-                <n-switch v-model:value="show">
-                  <template #checked>Hide Timeline</template>
-                  <template #unchecked>Show Timeline</template>
-                </n-switch>
-              <!-- </n-badge> -->
-              <n-collapse-transition :show="show">
-                <n-timeline>
-                  <n-timeline-item 
-                    v-for="timeline in person.timeline" 
-                    :key="timeline.key"
-                    :title="timeline.name"
-                    :content="timeline.text"
-                    :time="timeline.timestamp"
-                  />
-                </n-timeline>
-              </n-collapse-transition>
-            </n-space>
-          </n-tab-pane>
-        </n-tabs>
-      </n-layout>
+      <t-tabs />
     </n-layout>
     <t-footer />
   </n-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
-
 import { 
-  NButton,
-  NCollapseTransition,
   NLayout,
-  NProgress,
-  NSpace,
-  NSwitch,
-  NTabs,
-  NTabPane,
-  NTimeline,
-  NTimelineItem,
 } from 'naive-ui'
 
-import useScience from '../composables/useScience'
-import usePerson from '../composables/usePerson'
 import TFooter from '../components/TFooter.vue'
 import THeader from '../components/THeader.vue'
 import TSider from '../components/TSider.vue'
+import TTabs from '../components/TTabs.vue'
 
 export default {
   name: 'TGame',
   components: {
-    NButton,
-    NCollapseTransition,
     NLayout,
-    NProgress,
-    NSpace,
-    NSwitch,
-    NTabs,
-    NTabPane,
-    NTimeline,
-    NTimelineItem,
     TFooter,
     THeader,
     TSider,
+    TTabs,
   },
   setup () {
-    let { increment } = useScience();
-    let { personList } = usePerson();
-
     return {
-      increment,
-      show: ref(false),
-      personList,
     };
   }
 }
---
+++ src/components/TTabs.vue
@@ -0,0 +1,94 @@
+<template>
+  <n-tabs type="card" justify-content="space-evenly">
+    <n-tab-pane 
+      v-for="person in (personList.filter(person => person.isUnlocked))" 
+      :key="person.key"
+      :name="person.key" 
+      :tab="person.personName"
+    >
+      <n-space horizontal style="padding-left: 20px;">
+        <n-progress
+          type="multiple-circle"
+          :percentage="person.scienceList.filter(science => science.isUnlocked).map(({ current }) => current)"
+          :color="person.scienceList.filter(science => science.isUnlocked).map(({ color }) => color)"
+        >
+          Science
+        </n-progress>
+        <n-space vertical>
+          <n-button 
+            ghost
+            round
+            v-for="science in person.scienceList.filter(science => science.isUnlocked)"
+            :key="science.key"
+            :color="science.color"
+            @click="increment(science.key)"
+          >
+            {{science.label}}
+          </n-button>
+        </n-space>
+      </n-space>
+      <n-space vertical style="padding: 20px;">
+        <!-- <n-badge value=5> -->
+          <n-switch v-model:value="show">
+            <template #checked>Hide Timeline</template>
+            <template #unchecked>Show Timeline</template>
+          </n-switch>
+        <!-- </n-badge> -->
+        <n-collapse-transition :show="show">
+          <n-timeline>
+            <n-timeline-item 
+              v-for="timeline in person.timeline" 
+              :key="timeline.key"
+              :title="timeline.name"
+              :content="timeline.text"
+              :time="timeline.timestamp"
+            />
+          </n-timeline>
+        </n-collapse-transition>
+      </n-space>
+    </n-tab-pane>
+  </n-tabs>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+import { 
+  NButton,
+  NCollapseTransition,
+  NProgress,
+  NSpace,
+  NSwitch,
+  NTabs,
+  NTabPane,
+  NTimeline,
+  NTimelineItem,
+} from 'naive-ui'
+
+import useScience from '../composables/useScience'
+import usePerson from '../composables/usePerson'
+
+export default defineComponent({
+  components: {
+    NButton,
+    NCollapseTransition,
+    NProgress,
+    NSpace,
+    NSwitch,
+    NTabs,
+    NTabPane,
+    NTimeline,
+    NTimelineItem,
+  },
+  setup() {
+    let { increment } = useScience();
+    let { personList } = usePerson();
+
+    return {
+      increment,
+      personList,
+      show: ref(false),
+    }
+  },
+})
+</script>
