269471c7bce910a1a4e49d2e1628e4350b982e95
pbr4ne <pbr4ne@protonmail.com>
2022-01-05 15:58:47 -0800

    feat: add initial notification
    Not sure where this should ultimately live.

--- src/components/TGame.vue
+++ src/components/TGame.vue
@@ -15,7 +15,10 @@ import TFooter from '@/components/TFooter.vue'
 import THeader from '@/components/THeader.vue'
 import TSider from '@/components/TSider.vue'
 import TTabs from '@/components/TTabs.vue'
+import { Timeline } from '@/entities/Timeline'
 import useUnlockWatch from '@/composables/useUnlockWatch'
+import useInitialize from '@/composables/useInitialize'
+import { useNotification } from 'naive-ui'
 
 export default {
   components: {
@@ -27,6 +30,24 @@ export default {
   },
   setup () {
     useUnlockWatch();
+
+    //todo - eh should this be here?
+    const notification = useNotification();
+    const { personList } = useInitialize();
+
+    let initialMessage = new Timeline(
+      'lennox-old',
+      'lennox-old-1',
+      '1984',
+      'The year is 1984. You are Lennox, a physicist researching the most cutting-edge technology: Quantum Computing! With enough research, you should be able to build your own Quantum Computer. Let’s get started…',
+      '1984-03-05 20:24:00',
+    );
+    notification.create({
+      title: initialMessage.name,
+      content: initialMessage.text,
+      meta: initialMessage.timestamp,
+    });
+    personList.find(person => person.key === 'lennox-old').timeline.push(initialMessage);
 
     return {
     };
