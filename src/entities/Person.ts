13455de705a7c230a86bc4f3573a3fca3c5c9519
pbr4ne <pbr4ne@protonmail.com>
2022-01-05 17:36:49 -0800

    refactor: Person entity to TypeScript

---
+++ src/entities/Person.ts
@@ -0,0 +1,43 @@
+import { Science } from '@/entities/Science'
+import { Engineering } from '@/entities/Engineering'
+import { Timeline } from '@/entities/Timeline'
+
+export class Person {
+  key: String;
+  name: String;
+  year: String;
+  scienceList: Array<Science>;
+  engineeringList: Array<Engineering>;
+  devicesList: Array<Object>;
+  isUnlocked: Boolean;
+  timeline: Array<Timeline>;
+
+  constructor(key: String, name: String, year: String, scienceList: Array<Science>, 
+    engineeringList: Array<Engineering>, deviceList: Array<Object>, isUnlocked: Boolean) {
+    this.key = key;
+    this.name = name;
+    this.year = year;
+
+    this.scienceList = scienceList;
+    this.engineeringList = engineeringList;
+    this.devicesList = deviceList;
+
+    this.isUnlocked = isUnlocked;
+
+    this.timeline = new Array<Timeline>();
+  }
+
+  numUnread() {
+    let count = 0;
+    this.timeline.forEach(t => {
+      if(!t.isRead) {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  markRead() {
+    this.timeline.forEach(t => {t.isRead = true;});
+  }
+}
