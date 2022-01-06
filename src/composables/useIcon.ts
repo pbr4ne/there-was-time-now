4c13c2c71d9e791d2629fa2dc45fc384c9a12734
pbr4ne <pbr4ne@protonmail.com>
2022-01-05 17:39:15 -0800

    refactor: useIcon composable to TypeScript

--- src/composables/useIcon.ts
+++ src/composables/useIcon.ts
@@ -1,9 +1,9 @@
-import { h } from 'vue'
+import { h, Component } from 'vue'
 import { NIcon } from 'naive-ui'
 
 export default function useIcon() {
 
-  function renderIcon (icon) {
+  function renderIcon (icon: Component) {
     return () => h(NIcon, null, { default: () => h(icon) })
   }
 
