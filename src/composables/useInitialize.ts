8b220e9428ee2a17615aaa441d869d3a27739a38
pbr4ne <pbr4ne@protonmail.com>
2022-01-08 20:22:24 -0800

    feat: added all(?) characters and research
    Doesn't include messages yet

--- src/composables/useInitialize.ts
+++ src/composables/useInitialize.ts
@@ -13,7 +13,7 @@ import {
 } from '@vicons/fluent'
 
 import {
-  FlaskOutline as ChemistryIcon,
+  FlaskOutline as AlchemyIcon,
 } from '@vicons/ionicons5'
 
 import {
@@ -21,7 +21,8 @@ import {
 } from '@vicons/material'
 
 import {
-  Atom as QuantumMechanicsIcon,
+  Atom as PhysicsIcon,
+  CircleX as DefaultIcon,
 } from '@vicons/tabler'
 
 import { Device } from '@/entities/Device'
@@ -42,21 +43,51 @@ import {
 const personList : any = reactive({
   [PersonKey.LENNOX_OLD]: new Person(PersonKey.LENNOX_OLD, 'Lennox (1984)', 1984),
   [PersonKey.LENNOX_YOUNG]: new Person(PersonKey.LENNOX_YOUNG, 'Lennox (1934)', 1934),
+  [PersonKey.SAMA]: new Person(PersonKey.SAMA, 'Sama (904)', 904),
+  [PersonKey.ITOTIA]: new Person(PersonKey.ITOTIA, 'Itotia (374)', 374),
+  [PersonKey.NECHTAN]: new Person(PersonKey.NECHTAN, 'Nechtan (2524 BC)', 0), //todo - deal with year
 });
 
 const scienceList : any = reactive({
-  [ScienceKey.QUANTUM_MECHANICS]: new Science('Quantum Mechanics', shallowRef(QuantumMechanicsIcon), '#B10DC9', 10),
+  [ScienceKey.PHYSICS]: new Science('Physics', shallowRef(PhysicsIcon), '#B10DC9', 10),
   [ScienceKey.QUANTUM_COMPUTING]: new Science('Quantum Computing', shallowRef(QuantumComputingIcon), '#39CCCC', 5),
-  // [ScienceKey.BIOLOGY]: new Science('Biology', shallowRef(BiologyIcon), '#F012BE'),
-  // [ScienceKey.CHEMISTRY]: new Science('Chemistry', shallowRef(ChemistryIcon), '#FF4136'),
+
+  [ScienceKey.BIOLOGY]: new Science('Biology', shallowRef(BiologyIcon), '#F012BE', 10),
+  [ScienceKey.GENETIC_MEMORY]: new Science('Genetic Memory', shallowRef(DefaultIcon), '#FFFFFF', 5),
+
+  [ScienceKey.ALCHEMY]: new Science('Alchemy', shallowRef(AlchemyIcon), '#FF4136', 10),
+  [ScienceKey.CHRONOMOGRIFICATION]: new Science('Chronomogrification', shallowRef(DefaultIcon), '#FFFFFF', 5),
+
+  [ScienceKey.MATHEMATICS]: new Science('Mathematics', shallowRef(DefaultIcon), '#FFFFFF', 10),
+  [ScienceKey.TIMELESS_ALGEBRA]: new Science('Timeless Algebra', shallowRef(DefaultIcon), '#FFFFFF', 5),
+
+  [ScienceKey.ASTRONOMY]: new Science('Astronomy', shallowRef(DefaultIcon), '#FFFFFF', 10),
+  [ScienceKey.AETHERIC_HOROSCOPES]: new Science('Aetheric Horoscopes', shallowRef(DefaultIcon), '#FFFFFF', 5),
 });
 
 const engineeringList : any = reactive({
-  [EngineeringKey.QUANTUM_COMPUTER]: new Engineering('Quantum Computer', shallowRef(QuantumComputerIcon), '#01FF70', 0.5, 1.2),
+  [EngineeringKey.QUANTUM_COMPUTER]: new Engineering('Quantum Computer', shallowRef(QuantumComputerIcon), '#01FF70', 0.5, 1.1),
+  [EngineeringKey.CHRONOCRYSTALS]: new Engineering('Chronocrystals', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1),
+
+  [EngineeringKey.TELOMERE_STRETCHER]: new Engineering('Telomere Stretcher', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1.1),
+  [EngineeringKey.THE_FLUID]: new Engineering('The Fluid', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1),
+
+  [EngineeringKey.DISTILLER]: new Engineering('Distiller', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1.1),
+  [EngineeringKey.ELEMENT_ZERO]: new Engineering('Element Zero', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1),
+
+  [EngineeringKey.NEPOHUALTZINTZIN]: new Engineering('Nepohualtzintzin', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1.1),
+  [EngineeringKey.OBSIDIAN]: new Engineering('Obsidian', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1),
+
+  [EngineeringKey.ZODIAC_TALISMAN]: new Engineering('Zodiac Talisman', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1.1),
+  [EngineeringKey.SARSEN_STONES]: new Engineering('Sarsen Stones', shallowRef(DefaultIcon), '#FFFFFF', 0.5, 1),
 });
 
 const deviceList : any = reactive({
   [DeviceKey.CRYSTAL_SARCOPHAGUS]: new Device('Crystal Sarcophagus', shallowRef(CrystalSarcophagusIcon), '#FFDC00', 0.25),
+  [DeviceKey.OMEGAPERSON]: new Device('Omegaperson', shallowRef(DefaultIcon), '#FFFFFF', 0.25),
+  [DeviceKey.PHILOSOPHERS_STONE]: new Device('Philosopher\'s Stone', shallowRef(DefaultIcon), '#FFFFFF', 0.25),
+  [DeviceKey.TZOLKIN]: new Device('Tzolk\'in', shallowRef(DefaultIcon), '#FFFFFF', 0.25),
+  [DeviceKey.TIMEHENGE]: new Device('Timehenge', shallowRef(DefaultIcon), '#FFFFFF', 0.25),
 });
 
 const researchList : any = reactive({
@@ -66,43 +97,80 @@ const researchList : any = reactive({
 })
 
 function associateUnlocksToResearch() {
-  //quantum mechanics unlocks young lennox
-  // scienceList[ScienceKey.QUANTUM_MECHANICS].unlocks.push(new Unlock(UnlockKey.PERSON, PersonKey.LENNOX_YOUNG, PersonKey.LENNOX_YOUNG, 1, 
-  //   new Message('Young Lennox Unlocked', 'I am Lennox - an inventor. The year is 1934.')));
-  //quantum mechanics unlocks young lennox's first science (biology)
-  //scienceList[ScienceKey.QUANTUM_MECHANICS].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.BIOLOGY, PersonKey.LENNOX_YOUNG, 1, null!));
 
-  // scienceList[ScienceKey.QUANTUM_MECHANICS].unlocks.push(new Unlock(UnlockKey.MESSAGE, PersonKey.LENNOX_OLD, PersonKey.LENNOX_OLD, 2,
-  //   new Message('Testing Message', 'Testing Message')));
+  //Lennox (1984)
+  scienceList[ScienceKey.PHYSICS].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.QUANTUM_COMPUTING, PersonKey.LENNOX_OLD, 10, null!));
+  scienceList[ScienceKey.QUANTUM_COMPUTING].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.QUANTUM_COMPUTER, PersonKey.LENNOX_OLD, 10, null!));
+  engineeringList[EngineeringKey.QUANTUM_COMPUTER].unlocks.push(new Unlock(UnlockKey.PERSON, PersonKey.LENNOX_YOUNG, PersonKey.LENNOX_YOUNG, 1, null!));
+  engineeringList[EngineeringKey.QUANTUM_COMPUTER].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.BIOLOGY, PersonKey.LENNOX_YOUNG, 1, null!));
 
-  scienceList[ScienceKey.QUANTUM_MECHANICS].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.QUANTUM_COMPUTING, PersonKey.LENNOX_OLD, 5,
-  new Message('Quantum Computing Unlocked', 'You can now research Quantum Computing.')));
+  //Lennox (1934)
+  scienceList[ScienceKey.BIOLOGY].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.GENETIC_MEMORY, PersonKey.LENNOX_YOUNG, 10, null!));
+  scienceList[ScienceKey.GENETIC_MEMORY].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.TELOMERE_STRETCHER, PersonKey.LENNOX_YOUNG, 10, null!));
+  engineeringList[EngineeringKey.TELOMERE_STRETCHER].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.CRYSTAL_SARCOPHAGUS, PersonKey.LENNOX_OLD, 10, null!));
+  engineeringList[EngineeringKey.TELOMERE_STRETCHER].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.CHRONOCRYSTALS, PersonKey.LENNOX_OLD, 10, null!));
 
-  //quantum computing unlocks quantum computer
-  scienceList[ScienceKey.QUANTUM_COMPUTING].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.QUANTUM_COMPUTER, PersonKey.LENNOX_OLD, 5,
-    new Message('Quantum Computer Unlocked', 'You can now build Quantum Computers.')));
-  
-  //biology unlocks quantum computing
-  // scienceList[ScienceKey.BIOLOGY].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.QUANTUM_COMPUTING, PersonKey.LENNOX_OLD, 1,
-  //   new Message('Quantum Computing unlocked', 'You can now research Quantum Computing.')));
+  //Lennox (1984)
+  engineeringList[EngineeringKey.CHRONOCRYSTALS].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.OMEGAPERSON, PersonKey.LENNOX_YOUNG, 10, null!));
+  engineeringList[EngineeringKey.CHRONOCRYSTALS].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.THE_FLUID, PersonKey.LENNOX_YOUNG, 10, null!));
 
-  //quantum computer unlocks crystal sarcophagus
-  engineeringList[EngineeringKey.QUANTUM_COMPUTER].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.CRYSTAL_SARCOPHAGUS, PersonKey.LENNOX_OLD, 5,
-    new Message('Crystal Sarcophagus Unlocked', 'You can now construct the Crystal Sarcophagus.')));
+  //Lennox (1934)
+  engineeringList[EngineeringKey.THE_FLUID].unlocks.push(new Unlock(UnlockKey.PERSON, PersonKey.SAMA, PersonKey.SAMA, 10, null!));
+  engineeringList[EngineeringKey.THE_FLUID].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.ALCHEMY, PersonKey.SAMA, 10, null!));
 
-  //quantum computer unlocks chemistry
-  // engineeringList[EngineeringKey.QUANTUM_COMPUTER].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.CHEMISTRY, PersonKey.LENNOX_YOUNG, 1,
-  //   new Message('Chemistry unlocked', 'You can now research Chemistry.')));
+  //Sama (904)
+  scienceList[ScienceKey.ALCHEMY].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.CHRONOMOGRIFICATION, PersonKey.SAMA, 10, null!));
+  scienceList[ScienceKey.CHRONOMOGRIFICATION].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.DISTILLER, PersonKey.SAMA, 10, null!));
+  engineeringList[EngineeringKey.DISTILLER].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.PHILOSOPHERS_STONE, PersonKey.SAMA, 10, null!));
+  engineeringList[EngineeringKey.DISTILLER].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.ELEMENT_ZERO, PersonKey.SAMA, 10, null!));
+  engineeringList[EngineeringKey.ELEMENT_ZERO].unlocks.push(new Unlock(UnlockKey.PERSON, PersonKey.ITOTIA, PersonKey.ITOTIA, 10, null!));
+  engineeringList[EngineeringKey.ELEMENT_ZERO].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.MATHEMATICS, PersonKey.ITOTIA, 10, null!));
+
+  //Itotia (374)
+  scienceList[ScienceKey.MATHEMATICS].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.TIMELESS_ALGEBRA, PersonKey.ITOTIA, 10, null!));
+  scienceList[ScienceKey.TIMELESS_ALGEBRA].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.NEPOHUALTZINTZIN, PersonKey.ITOTIA, 10, null!));
+  engineeringList[EngineeringKey.NEPOHUALTZINTZIN].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.TZOLKIN, PersonKey.ITOTIA, 10, null!));
+  engineeringList[EngineeringKey.NEPOHUALTZINTZIN].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.OBSIDIAN, PersonKey.ITOTIA, 10, null!));
+  engineeringList[EngineeringKey.OBSIDIAN].unlocks.push(new Unlock(UnlockKey.PERSON, PersonKey.NECHTAN, PersonKey.NECHTAN, 10, null!));
+  engineeringList[EngineeringKey.OBSIDIAN].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.ASTRONOMY, PersonKey.NECHTAN, 10, null!));
+
+  //Nechtan (2524 BC)
+  scienceList[ScienceKey.ASTRONOMY].unlocks.push(new Unlock(UnlockKey.SCIENCE, ScienceKey.AETHERIC_HOROSCOPES, PersonKey.NECHTAN, 10, null!));
+  scienceList[ScienceKey.AETHERIC_HOROSCOPES].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.ZODIAC_TALISMAN, PersonKey.NECHTAN, 10, null!));
+  engineeringList[EngineeringKey.ZODIAC_TALISMAN].unlocks.push(new Unlock(UnlockKey.DEVICE, DeviceKey.TIMEHENGE, PersonKey.NECHTAN, 10, null!));
+  engineeringList[EngineeringKey.ZODIAC_TALISMAN].unlocks.push(new Unlock(UnlockKey.ENGINEERING, EngineeringKey.SARSEN_STONES, PersonKey.NECHTAN, 10, null!));
 }
 
 function associateResearchToPeople() {
-  personList[PersonKey.LENNOX_OLD].scienceList.push(scienceList[ScienceKey.QUANTUM_MECHANICS] as Science);
+  personList[PersonKey.LENNOX_OLD].scienceList.push(scienceList[ScienceKey.PHYSICS] as Science);
   personList[PersonKey.LENNOX_OLD].scienceList.push(scienceList[ScienceKey.QUANTUM_COMPUTING] as Science);
   personList[PersonKey.LENNOX_OLD].engineeringList.push(engineeringList[EngineeringKey.QUANTUM_COMPUTER] as Engineering);
+  personList[PersonKey.LENNOX_OLD].engineeringList.push(engineeringList[EngineeringKey.CHRONOCRYSTALS] as Engineering);
   personList[PersonKey.LENNOX_OLD].deviceList.push(deviceList[DeviceKey.CRYSTAL_SARCOPHAGUS] as Device);
   
   personList[PersonKey.LENNOX_YOUNG].scienceList.push(scienceList[ScienceKey.BIOLOGY] as Science);
-  personList[PersonKey.LENNOX_YOUNG].scienceList.push(scienceList[ScienceKey.CHEMISTRY] as Science);
+  personList[PersonKey.LENNOX_YOUNG].scienceList.push(scienceList[ScienceKey.GENETIC_MEMORY] as Science);
+  personList[PersonKey.LENNOX_YOUNG].engineeringList.push(engineeringList[EngineeringKey.TELOMERE_STRETCHER] as Engineering);
+  personList[PersonKey.LENNOX_YOUNG].engineeringList.push(engineeringList[EngineeringKey.THE_FLUID] as Engineering);
+  personList[PersonKey.LENNOX_YOUNG].deviceList.push(deviceList[DeviceKey.OMEGAPERSON] as Device);
+
+  personList[PersonKey.SAMA].scienceList.push(scienceList[ScienceKey.ALCHEMY] as Science);
+  personList[PersonKey.SAMA].scienceList.push(scienceList[ScienceKey.CHRONOMOGRIFICATION] as Science);
+  personList[PersonKey.SAMA].engineeringList.push(engineeringList[EngineeringKey.DISTILLER] as Engineering);
+  personList[PersonKey.SAMA].engineeringList.push(engineeringList[EngineeringKey.ELEMENT_ZERO] as Engineering);
+  personList[PersonKey.SAMA].deviceList.push(deviceList[DeviceKey.PHILOSOPHERS_STONE] as Device);
+
+  personList[PersonKey.ITOTIA].scienceList.push(scienceList[ScienceKey.MATHEMATICS] as Science);
+  personList[PersonKey.ITOTIA].scienceList.push(scienceList[ScienceKey.TIMELESS_ALGEBRA] as Science);
+  personList[PersonKey.ITOTIA].engineeringList.push(engineeringList[EngineeringKey.NEPOHUALTZINTZIN] as Engineering);
+  personList[PersonKey.ITOTIA].engineeringList.push(engineeringList[EngineeringKey.OBSIDIAN] as Engineering);
+  personList[PersonKey.ITOTIA].deviceList.push(deviceList[DeviceKey.TZOLKIN] as Device);
+
+  personList[PersonKey.NECHTAN].scienceList.push(scienceList[ScienceKey.ASTRONOMY] as Science);
+  personList[PersonKey.NECHTAN].scienceList.push(scienceList[ScienceKey.AETHERIC_HOROSCOPES] as Science);
+  personList[PersonKey.NECHTAN].engineeringList.push(engineeringList[EngineeringKey.ZODIAC_TALISMAN] as Engineering);
+  personList[PersonKey.NECHTAN].engineeringList.push(engineeringList[EngineeringKey.SARSEN_STONES] as Engineering);
+  personList[PersonKey.NECHTAN].deviceList.push(deviceList[DeviceKey.TIMEHENGE] as Device);
 }
 
 associateResearchToPeople();
@@ -110,7 +178,7 @@ associateUnlocksToResearch();
 
 export default function useInitialize() {
 
-  scienceList[ScienceKey.QUANTUM_MECHANICS].isUnlocked = true;
+  scienceList[ScienceKey.PHYSICS].isUnlocked = true;
   personList[PersonKey.LENNOX_OLD].isUnlocked = true;
 
   //todo - remove this later
--- src/enum/Enums.ts
+++ src/enum/Enums.ts
@@ -9,19 +9,49 @@ export enum UnlockKey {
 export enum PersonKey {
   LENNOX_OLD = "lennox-old",
   LENNOX_YOUNG = "lennox-young",
+  SAMA = "sama",
+  ITOTIA = "itotia",
+  NECHTAN = "nechtan",
 }
 
 export enum ScienceKey {
-  QUANTUM_MECHANICS = "quantum-mechanics",
+  PHYSICS = "physics",
   QUANTUM_COMPUTING = "quantum-computing",
+
   BIOLOGY = "biology",
-  CHEMISTRY = "chemistry",
+  GENETIC_MEMORY = "genetic-memory",
+
+  ALCHEMY = "alchemy",
+  CHRONOMOGRIFICATION = "chronomogrification",
+
+  MATHEMATICS = "mathematics",
+  TIMELESS_ALGEBRA = "timeless-algebra",
+
+  ASTRONOMY = "astronomy",
+  AETHERIC_HOROSCOPES = "aetheric-horoscopes",
 }
 
 export enum EngineeringKey {
   QUANTUM_COMPUTER = "quantum-computer",
+  CHRONOCRYSTALS = "chronocrystals",
+
+  TELOMERE_STRETCHER = "telomere-stretcher",
+  THE_FLUID = "the-fluid",
+
+  DISTILLER = "distiller",
+  ELEMENT_ZERO = "element-zero",
+
+  NEPOHUALTZINTZIN = "nepohualtzintzin",
+  OBSIDIAN = "obsidian",
+
+  ZODIAC_TALISMAN = "zodiac-talisman",
+  SARSEN_STONES = "sarsen-stones",
 }
 
 export enum DeviceKey {
   CRYSTAL_SARCOPHAGUS = "crystal-sarcophagus",
+  OMEGAPERSON = "omegaperson",
+  PHILOSOPHERS_STONE = "philosophers-stone",
+  TZOLKIN = "tzolkin",
+  TIMEHENGE = "timehenge",
 }
