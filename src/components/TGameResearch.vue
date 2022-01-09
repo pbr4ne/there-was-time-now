ecd7dd8947d5a2e877b02743ad52e44c175e5323
pbr4ne <pbr4ne@protonmail.com>
2022-01-09 13:42:07 -0800

    feat: add scrollbar to table

--- src/components/TGameResearch.vue
+++ src/components/TGameResearch.vue
@@ -8,85 +8,86 @@
       <h3>{{label}}</h3>
     </n-progress>
     <n-space vertical>
-      <n-table 
-        :bordered="false" 
-        :single-line="true" 
-        :single-column="true" 
-        :size="'small'"
-      >
-        <thead>
-          <tr>
-            <th>
-              Research
-            </th>
-            <th style="text-align: center;">
-              <n-button size="tiny">
-                ×10
-              </n-button>
-            </th>
-            <th style="text-align: center;">
-              Undergrads
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr 
-            v-for="research in researchList.filter(research => research.isUnlocked)"
-            :key="research.label"
-          >
-            <td>
-              <t-game-research-button 
-                ghost
-                round
-                :color="research.color"
-                @click="incrementResearch(research)"
-                :research = "research">
-              </t-game-research-button>
-            </td>
-            <td>
-              <n-popover trigger="hover">
-                <template #trigger>
-                  <n-button>
-                    Sell
-                  </n-button>
-                </template>
-                <span>Not implemented yet</span>
-              </n-popover>
-            </td>
-            <td>
-              <n-popover trigger="hover">
-                <template #trigger>
-                  <n-button-group size="small">
-                    <n-button round>
-                      <template #icon>
-                        <n-icon><minus-icon /></n-icon>
-                      </template>
+      <n-scrollbar x-scrollable>
+        <n-table 
+          :bordered="false" 
+          :single-line="true" 
+          :single-column="true" 
+          :size="'small'"
+        >
+          <thead>
+            <tr>
+              <th>
+                Research
+              </th>
+              <th style="text-align: center;">
+                <n-button size="tiny">
+                  ×10
+                </n-button>
+              </th>
+              <th style="text-align: center;">
+                Undergrads
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr 
+              v-for="research in researchList.filter(research => research.isUnlocked)"
+              :key="research.label"
+            >
+              <td>
+                <t-game-research-button 
+                  ghost
+                  round
+                  :color="research.color"
+                  @click="incrementResearch(research)"
+                  :research = "research">
+                </t-game-research-button>
+              </td>
+              <td>
+                <n-popover trigger="hover">
+                  <template #trigger>
+                    <n-button>
+                      Sell
                     </n-button>
-                    <n-button disabled style="width: 40px">
-                      5
-                    </n-button>
-                    <n-button round>
-                      <template #icon>
-                        <n-icon><plus-icon /></n-icon>
-                      </template>
-                    </n-button>
-                    
-                  </n-button-group>
                   </template>
-                <span>Not implemented yet</span>
-              </n-popover>
-            </td>
-          </tr>
-        </tbody>
-      </n-table>
-      
+                  <span>Not implemented yet</span>
+                </n-popover>
+              </td>
+              <td>
+                <n-popover trigger="hover">
+                  <template #trigger>
+                    <n-button-group size="small">
+                      <n-button round>
+                        <template #icon>
+                          <n-icon><minus-icon /></n-icon>
+                        </template>
+                      </n-button>
+                      <n-button disabled style="width: 40px">
+                        5
+                      </n-button>
+                      <n-button round>
+                        <template #icon>
+                          <n-icon><plus-icon /></n-icon>
+                        </template>
+                      </n-button>
+                      
+                    </n-button-group>
+                    </template>
+                  <span>Not implemented yet</span>
+                </n-popover>
+              </td>
+            </tr>
+          </tbody>
+        </n-table>
+      </n-scrollbar>
     </n-space>
   </n-space>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { NButton, NIcon, NButtonGroup, NPopover, NProgress, NSpace, NTable } from 'naive-ui'
+import { NButton, NIcon, NButtonGroup, NPopover, NProgress, NScrollbar, NSpace, NTable } from 'naive-ui'
 import {
   MinusOutlined as MinusIcon,
   PlusOutlined as PlusIcon,
@@ -101,6 +102,7 @@ export default defineComponent({
     NIcon,
     NPopover,
     NProgress,
+    NScrollbar,
     NSpace,
     NTable,
     PlusIcon,
