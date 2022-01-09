e2e44fb7a097d5526920e19e522f4886aa10e502
pbr4ne <pbr4ne@protonmail.com>
2022-01-09 10:56:26 -0800

    feat: add header and different fonts

--- src/components/TGameHeader.vue
+++ src/components/TGameHeader.vue
@@ -1,26 +1,31 @@
 <template>
   <n-layout-header bordered style="padding-top: 5px;">
-    <n-space justify="end" style="height: 72.8px">
-      <span></span>
-      <n-statistic label="Time Expansion" v-if="countdownTimer.isRunning()">
-        {{expandConstant.toFixed(2)}}
-      </n-statistic>
 
-      <n-statistic label="Days Left" v-if="countdownTimer.isRunning()">
-        {{timeLeft}}
-      </n-statistic>
+    <n-space justify="space-between" class="twtnHeader">
+      <n-space style="padding-left: 5px;" width="100%">
+        <span :class="[titleFont]" v-if="showTitle">There Was Time Now</span>
+      </n-space>
 
-      <!-- eslint-disable-next-line vue/valid-v-slot -->
-      <n-spin #icon style="vertical-align: middle;" v-if="countdownTimer.isRunning()">
-        <n-icon><time-icon /></n-icon>
-      </n-spin>
+      <n-space width="209px">
+        <n-statistic label="Time Expansion" v-if="countdownTimer.isRunning()">
+          {{expandConstant.toFixed(2)}}
+        </n-statistic>
 
+        <n-statistic label="Days Left" v-if="countdownTimer.isRunning()">
+          {{timeLeft}}
+        </n-statistic>
+
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <n-spin #icon style="vertical-align: middle;" v-if="countdownTimer.isRunning()">
+          <n-icon><time-icon /></n-icon>
+        </n-spin>
+      </n-space>
     </n-space>
   </n-layout-header>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 
 import { 
   NIcon,
@@ -47,11 +52,84 @@ export default defineComponent({
   },
   setup() {
     const { countdownTimer, expandConstant, timeLeft } = useTime();
+
+    //don't show title for smaller widths until I can figure out 
+    let showTitle = window.innerWidth > 700;
+
+    const titleFont = computed(() => {
+      return {
+        "title1984": true,
+        "titleText": true,
+      }
+    });
+
     return {
       countdownTimer,
       expandConstant,
+      showTitle,
       timeLeft,
+      titleFont,
     }
   },
 })
 </script>
+<style scoped>
+  @font-face {
+    font-family: "1984";
+    src: local("lazenby-computer.liquid"),
+    url("@/fonts/lazenby-computer.liquid.ttf")
+    format("truetype");
+  }
+
+  @font-face {
+    font-family: "1934";
+    src: local("Joe Caps Underwood"),
+    url("@/fonts/Joe Caps Underwood.ttf")
+    format("truetype");
+  }
+
+   @font-face {
+    font-family: "904";
+    src: local("Ramadhan Karim"),
+    url("@/fonts/Ramadhan Karim.ttf")
+    format("truetype");
+  }
+
+   @font-face {
+    font-family: "374";
+    src: local("Maya Culpa"),
+    url("@/fonts/Maya Culpa.ttf")
+    format("truetype");
+  }
+
+   @font-face {
+    font-family: "2524 BC";
+    src: local("moria-citadel-regular"),
+    url("@/fonts/moria-citadel.regular.ttf")
+    format("truetype");
+  }
+
+  .titleText {
+    font-size: 40px;
+  }
+
+  .title1984 {
+    font-family: "1984";
+  }
+
+  .title1934 {
+    font-family: "1934";
+  }
+
+  .title934 {
+    font-family: "904";
+  }
+
+  .title374 {
+    font-family: "374"
+  }
+  
+  .twtnHeader {
+    
+  }
+</style>
