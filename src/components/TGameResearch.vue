6f5f7aaa430cd1d5196f719b89ab610841c137bb
pbr4ne <pbr4ne@protonmail.com>
2022-01-09 13:20:15 -0800

    feat: prototype UI for selling & automating

--- src/components/TGameResearch.vue
+++ src/components/TGameResearch.vue
@@ -8,28 +8,102 @@
       <h3>{{label}}</h3>
     </n-progress>
     <n-space vertical>
-      <t-game-research-button 
-        ghost
-        round
-        v-for="research in researchList.filter(research => research.isUnlocked)"
-        :key="research.label"
-        :color="research.color"
-        @click="incrementResearch(research)"
-        :research = "research">
-      </t-game-research-button >
+      <n-table 
+        :bordered="false" 
+        :single-line="true" 
+        :single-column="true" 
+        :size="'small'"
+      >
+        <thead>
+          <tr>
+            <th>
+              Research
+            </th>
+            <th style="text-align: center;">
+              <n-button size="tiny">
+                ×10
+              </n-button>
+            </th>
+            <th style="text-align: center;">
+              Undergrads
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="research in researchList.filter(research => research.isUnlocked)"
+            :key="research.label"
+          >
+            <td>
+              <t-game-research-button 
+                ghost
+                round
+                :color="research.color"
+                @click="incrementResearch(research)"
+                :research = "research">
+              </t-game-research-button>
+            </td>
+            <td>
+              <n-popover trigger="hover">
+                <template #trigger>
+                  <n-button>
+                    Sell
+                  </n-button>
+                </template>
+                <span>Not implemented yet</span>
+              </n-popover>
+            </td>
+            <td>
+              <n-popover trigger="hover">
+                <template #trigger>
+                  <n-button-group size="small">
+                    <n-button round>
+                      <template #icon>
+                        <n-icon><minus-icon /></n-icon>
+                      </template>
+                    </n-button>
+                    <n-button disabled style="width: 40px">
+                      5
+                    </n-button>
+                    <n-button round>
+                      <template #icon>
+                        <n-icon><plus-icon /></n-icon>
+                      </template>
+                    </n-button>
+                    
+                  </n-button-group>
+                  </template>
+                <span>Not implemented yet</span>
+              </n-popover>
+            </td>
+          </tr>
+        </tbody>
+      </n-table>
+      
     </n-space>
   </n-space>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { NProgress, NSpace } from 'naive-ui'
+import { NButton, NIcon, NButtonGroup, NPopover, NProgress, NSpace, NTable } from 'naive-ui'
+import {
+  MinusOutlined as MinusIcon,
+  PlusOutlined as PlusIcon,
+} from '@vicons/material'
 import useResearch from '@/composables/useResearch'
 
 export default defineComponent({
   components: {
+    MinusIcon,
+    NButton,
+    NButtonGroup,
+    NIcon,
+    NPopover,
     NProgress,
     NSpace,
+    NTable,
+    PlusIcon,
   },
   props: {
     label: String,
