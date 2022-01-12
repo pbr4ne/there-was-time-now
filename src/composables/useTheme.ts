43bbdcdcba1d1f4cca93905cdc18f3f7ab4369a2
pbr4ne <pbr4ne@protonmail.com>
2022-01-12 10:47:08 -0800

    feat: change some research colours when theme changes
    Some are not visible in light mode

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -1,17 +1,27 @@
 import { ref } from 'vue'
+import useInitialize from '@/composables/useInitialize'
+import { ScienceKey } from '@/enum/Enums'
 
 const lightMode = ref(false);
 const otherThemeName = ref('Light Mode');
 
 export default function useTheme() {
 
+  const { scienceList } = useInitialize();
+
+  //todo - change colours more dynamically
   const switchTheme = () => {
     if(lightMode.value) {
       lightMode.value = false;
       otherThemeName.value = "Light Mode";
+      scienceList[ScienceKey.GENETIC_MEMORY].color = '#FEFAE0';
+      scienceList[ScienceKey.CHRONOMOGRIFICATION].color = '#FFFFFF';
+      
     } else {
       lightMode.value = true;
       otherThemeName.value = "Dark Mode";
+      scienceList[ScienceKey.GENETIC_MEMORY].color = '#826107';
+      scienceList[ScienceKey.CHRONOMOGRIFICATION].color = '#363431';
     }
   }
 
