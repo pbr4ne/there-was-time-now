ee0f9028f6569f719d1220feb683fc97c1fa7f25
pbr4ne <pbr4ne@protonmail.com>
2022-01-12 19:20:29 -0800

    fix: make popover not cut off on small screens

--- src/components/TGameSiderItem.vue
+++ src/components/TGameSiderItem.vue
@@ -1,9 +1,14 @@
 <template>
-  <n-popover trigger="hover" placement="right" :style="{ maxWidth: '200px' }">
+  <n-popover trigger="hover" :placement="placement()" :style="{ maxWidth: '200px' }">
    <template #trigger>
-      <div>{{research.label}} <b :style="{ color: research.color }">{{research.total}}</b></div>
+      <div>
+        {{research.label}} <b :style="{ color: research.color }">{{research.total}}</b>
+      </div>
    </template>
-   <span>{{messages[research.key].messageSections[0].text}}</span>
+   <div>
+    <span :style="{color: research.color}">{{research.label}}</span><br />
+     {{messages[research.key].messageSections[0].text}}
+    </div>
   </n-popover>
 </template>
 
@@ -21,8 +26,10 @@ export default defineComponent({
     research: Research,
   },
   setup () {
+    const placement = () => window.innerWidth > 700 ? "right" : "bottom";
     return {
       messages,
+      placement,
     }
   }
 })
