c0a3b5b5bdf9f8f618884712826777d95807484d
pbr4ne <pbr4ne@protonmail.com>
2022-01-20 18:17:20 -0800

    build: just reorganizing things

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript',
+  ],
+  parser: "@typescript-eslint/parser",
+  rules: {
+  }
+}
