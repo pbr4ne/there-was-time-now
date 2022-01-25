4f95af4c0cf3675f5e8c9cbf934ed2aa9a5c56c8
pbr4ne <pbr4ne@protonmail.com>
2022-01-24 16:27:26 -0800

    refactor: move around some functions in useMessage

--- src/composables/useMessage.ts
+++ src/composables/useMessage.ts
@@ -7,27 +7,9 @@ import useTime from '@/composables/useTime'
 import { PersonKey,  NarrativeKey } from '@/enum/Enums'
 import { messages } from '@/locales/en'
 import { Message } from '@/entities/Message'
-import { DialogApiInjection } from 'naive-ui/lib/dialog/src/DialogProvider'
 import { Person } from '@/entities/Person'
+
 const showTimeline = ref(false);
-
-function createNotification(message: Message, dialog: DialogApiInjection) {
-  dialog.create({
-    title: message.title,
-    content: () => h(TGameMessage, { messageSections: message.messageSections}),
-    positiveText: "OK!",
-    maskClosable: false,
-    icon: () => h(NIcon, null, { default: () => h(TimeIcon) })
-  });
-}
-
-function setTimestamp(message: Message, year: number, timeElapsed: any) {
-  const d = new Date(year, 0);
-  d.setDate(d.getDate() + timeElapsed.value);
-  message.timestamp = d.toISOString().split('T')[0];
-  //todo - this is hacky
-  message.timestamp = message.timestamp.replace('1900-', '2524 BC ');
-}
 
 export default function useMessage() {
   const dialog = useDialog();
@@ -37,9 +19,27 @@ export default function useMessage() {
   function sendMessage(message: Message, person: Person) {
     if(!message.wasSent){
       setTimestamp(message, person.year, timeElapsed);
-      createNotification(message, dialog);
+      createNotification(message);
       person.messageList.unshift(message);
     }
+  }
+
+  function createNotification(message: Message) {
+    dialog.create({
+      title: message.title,
+      content: () => h(TGameMessage, { messageSections: message.messageSections}),
+      positiveText: "OK!",
+      maskClosable: false,
+      icon: () => h(NIcon, null, { default: () => h(TimeIcon) })
+    });
+  }
+  
+  function setTimestamp(message: Message, year: number, timeElapsed: any) {
+    const d = new Date(year, 0);
+    d.setDate(d.getDate() + timeElapsed.value);
+    message.timestamp = d.toISOString().split('T')[0];
+    //todo - this is hacky
+    message.timestamp = message.timestamp.replace('1900-', '2524 BC ');
   }
 
   const sendInitialMessage = () => sendNarrativeMessage(messages[NarrativeKey.INTRO]);
