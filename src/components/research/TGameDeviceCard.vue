64cf53dc2c6f7a23482478cfb5343d364712a232
pbr4ne <pbr4ne@protonmail.com>
2022-01-31 18:13:22 -0800

    refactor: change from render functions to templates

--- src/components/research/TGameDeviceCard.vue
+++ src/components/research/TGameDeviceCard.vue
@@ -1,3 +1,13 @@
+<template>
+  <n-popover :style="{ maxWidth: '200px' }" trigger="hover" placement="top-start">
+    <template #trigger>
+      <n-card :style="cardStyle()" @click="startResearch(research)">
+        <t-game-device-progress :research="research" />
+      </n-card>
+    </template>
+    <t-game-research-needs :research="research" />
+  </n-popover>
+</template>
 <script>
 import { defineComponent, h } from 'vue'
 import { NCard, NPopover } from 'naive-ui'
@@ -7,44 +17,32 @@ import useResearch from '@/composables/useResearch'
 import { Research } from '@/entities/Research'
 
 export default defineComponent({
+  components: {
+    NCard,
+    NPopover,
+    TGameDeviceProgress,
+    TGameResearchNeeds,
+  },
   props: {
     research: Research
   },
   setup(props) {
     const { canIncrementResearch, incrementResearch } = useResearch();
 
-    const research = (research) => {
+    const cardStyle = () => {
+      return { cursor: canIncrementResearch(props.research) || props.research.total == 1 ? "pointer" : "not-allowed" }
+    }
+
+    const startResearch = (research) => {
       if(canIncrementResearch(research)) {
         return incrementResearch(research);
-      } else {
-        return;
       }
     }
 
-    return () => 
-    h(
-      NPopover, 
-      {
-        style: { maxWidth: '200px' },
-        trigger: 'hover',
-        placement: 'top-start',
-      }, 
-      {
-        trigger: () => h(
-          NCard, 
-          { 
-            style: { 
-              cursor: canIncrementResearch(props.research) || props.research.total == 1 ? "pointer" : "not-allowed"
-            },
-            onClick: () => research(props.research)
-          },
-          {
-            default: () => h(TGameDeviceProgress, { research: props.research })
-          } 
-        ),
-        default: () => h(TGameResearchNeeds, { research: props.research })
-      }
-    )
+    return {
+      cardStyle,
+      startResearch,
+    }
   },
 })
 </script>
--- src/components/research/TGameResearchButton.vue
+++ src/components/research/TGameResearchButton.vue
@@ -1,3 +1,19 @@
+<template>
+  <n-button 
+    ghost 
+    round 
+    :color="research.color" 
+    :disabled="!canIncrementResearch(research)" 
+    @click="incrementResearch(research)"
+  >
+    <template #icon>
+      <component :is="renderIcon(research.icon)" />
+    </template>
+    <span v-if="displayButtonText()">
+      {{ research.label }}
+    </span>
+  </n-button>
+</template>
 <script>
 import { defineComponent, h } from 'vue'
 import { NButton } from 'naive-ui'
@@ -6,29 +22,30 @@ import useResearch from '@/composables/useResearch'
 import { Research } from '@/entities/Research'
 
 export default defineComponent({
+  components: {
+    NButton,
+  },
   props: {
     research: Research,
-    disabled: Boolean,
   },
-  setup(props) {
+  setup() {
     const { sellFeatureEnabled } = useFlags();
     const { canIncrementResearch, incrementResearch } = useResearch();
 
-    return () => 
-    h(
-      NButton, 
-      { 
-        ghost: true, 
-        round: true,
-        color: props.research.color,
-        disabled: !canIncrementResearch(props.research),
-        onClick: () => incrementResearch(props.research)
-      }, 
-      {
-        icon: () => h(props.research.icon),
-        default: () => h('span', null, window.innerWidth > 700 || !sellFeatureEnabled.value ? props.research.label: null),
-      }
-    );
+    const displayButtonText = () => {
+      return window.innerWidth > 700 || !sellFeatureEnabled.value;
+    }
+
+    const renderIcon = (icon) => {
+      return h(icon)
+    }
+
+    return {
+      canIncrementResearch,
+      displayButtonText,
+      incrementResearch,
+      renderIcon,
+    }
   },
 })
 </script>
