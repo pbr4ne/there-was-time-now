7c5262928aff533f2972e26ee8272341573b56d1
pbr4ne <pbr4ne@protonmail.com>
2022-03-06 12:15:05 -0800

    feat: added stuff to about

--- src/components/settings/TGameAbout.vue
+++ src/components/settings/TGameAbout.vue
@@ -3,18 +3,54 @@
   <br><br>
   this is a game about "science" and "time"
   <br><br>
-  programming by <span style="color: #00FF00">pbrane</span>
-  <br><br>
-  writing by James Funfer 
-  <a 
-    href="https://jamesfunfer.com/" 
-    style="color: #F72585" 
-    target="_blank"
-    rel="noopener"
-  >
-    author page
-  </a>
-  <br><br>
+  programming by pbrane
+  <div class="indent">
+    <a 
+      href="https://github.com/pbr4ne" 
+      style="color: #00FF00" 
+      target="_blank"
+      rel="noopener"
+    >
+      github
+    </a><br>
+    <a 
+      href="https://pbrane.itch.io/" 
+      style="color: #00FF00" 
+      target="_blank"
+      rel="noopener"
+    >
+      itch.io
+    </a><br>
+    <a 
+      href="https://plaza.dsolver.ca/m/pbrane_dev" 
+      style="color: #00FF00" 
+      target="_blank"
+      rel="noopener"
+    >
+      incremental games plaza
+    </a>
+  </div>
+  <br>
+  writing by James Funfer
+  <div class="indent">
+    <a 
+      href="https://jamesfunfer.com/" 
+      style="color: #F72585" 
+      target="_blank"
+      rel="noopener"
+    >
+      author page
+    </a><br>
+    <a 
+      href="https://www.amazon.com/Crystal-Promise-Shattered-Book/dp/0615665195" 
+      style="color: #F72585" 
+      target="_blank"
+      rel="noopener"
+    >
+      Crystal Promise: The Shattered Crystal: Book 1
+    </a>
+  </div>
+  <br>
   special thanks to: 
   <div class="indent">
     goodjukel<br>
